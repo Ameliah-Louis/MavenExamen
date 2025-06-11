9d07db04b263d30eab6116141f46a190837a5592
Orzhov <alouis@stagiaire-humanbooster.com>
2025-06-11 15:09:08 +0200

    first commit

---
+++ src/main/java/hb/exam/App.java
@@ -0,0 +1,13 @@
+package hb.exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
